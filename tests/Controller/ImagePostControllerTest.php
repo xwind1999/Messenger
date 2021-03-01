555ca19b9bad8c3f7625c14ea3870614d2849df7
xwind1999 <playgoodgame7@gmail.com>
2021-03-01 18:06:35 +0700

    Install phpunit

---
+++ tests/Controller/ImagePostControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ImagePostControllerTest extends WebTestCase
+{
+    public function testCreate()
+    {
+        $this->assertEquals(42, 42);
+    }
+}
